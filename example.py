8f13593fb7d8d4ed3c2125273573d809a429c844
Gustavo Marttos <marttosc@gmail.com>
2016-10-12 16:03:16 -0300

    A simple example using the PyStackExchange library.

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 from stackhub.Stackhub import Stackhub
 from stackhub.Database import Database
+import stackexchange
 
 sh = Stackhub()
 
@@ -9,3 +10,8 @@ cursor = db.get().stackhub.find()
 
 for document in cursor:
     print(document)
+
+so = stackexchange.Site(stackexchange.StackOverflow, sh.env('STACKAPP_KEY'))
+
+# Random user.
+print(so.user(4032278).display_name)
