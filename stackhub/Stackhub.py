24661a4b44992a93401a6961cb8046e6dd73b212
Gustavo Marttos <marttosc@gmail.com>
2016-10-15 20:33:10 -0300

    Save the Github Trends data into MongoDB.

--- stackhub/Stackhub.py
+++ stackhub/Stackhub.py
@@ -1,9 +1,25 @@
+from stackhub.Database import Database
 from stackhub.Environment import Environment
-
+from stackhub.Static import Trend
 
 class Stackhub:
     def __init__(self):
         self.environ = Environment()
 
+        self.db = Database(database = self.env('MONGO_DATABASE'), host = self.env('MONGO_HOST')).get()
+
     def env(self, key, default=None):
         return self.environ.get_env(key, default)
+
+    def github_trends(self):
+        """
+        Return and save the Github Trends data into MongoDB.
+        """
+        trends = Trend().load()
+
+        # Truncate the github_trends collection.
+        self.db.github_trends.delete_many({})
+
+        self.db.github_trends.insert_many(trends)
+
+        return trends
