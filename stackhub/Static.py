22e39ee63183eac79a79f1bfb40d8a1d505dcaeb
Gustavo Marttos <marttosc@gmail.com>
2016-10-15 14:18:25 -0300

    Get the Github Trends repositories statically.

---
+++ stackhub/Static.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+from pymongo import common, MongoClient
+
+import requests
+
+
+class Trend(object):
+    GITHUB_URL = 'https://github.com/showcases/programming-languages'
+
+    def load(object):
+        r = requests.get(Trend.GITHUB_URL)
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        items = soup.find_all('li', class_='repo-list-item')
+
+        components = []
+
+        for item in items:
+            info = list(filter(None, item.div.get_text().split('\n')))
+            info = list(filter(None, [i.strip() for i in info]))
+
+            repository = ''.join(list(filter(None, [x.strip() for x in item.h3.text.split('\n')])))
+
+            if len(info) == 2:
+                language = None
+            else:
+                language = info[0]
+
+            stars = int(info[0 if len(info) == 2 else 1].replace(',', ''))
+            forks = int(info[1 if len(info) == 2 else 2].replace(',', ''))
+
+            components.append(TrendItem(repository, language, stars, forks))
+
+        return components
+
+class TrendItem(object):
+    def __init__(self, repository, language, stars, forks):
+        self.repository = repository
+        self.language = language
+        self.stars = stars
+        self.forks = forks
