59e0bd1b758f99c196e10db3ca53ee0569ebbbe4
Gustavo Marttos <marttosc@gmail.com>
2016-10-15 20:34:59 -0300

    Example to load the Github Trends statically.

--- example.py
+++ example.py
@@ -1,17 +1,7 @@
 from stackhub.Stackhub import Stackhub
-from stackhub.Database import Database
-import stackexchange
 
 sh = Stackhub()
 
-db = Database(database = sh.env('MONGO_DATABASE'), host = sh.env('MONGO_HOST'))
+trends = sh.github_trends()
 
-cursor = db.get().stackhub.find()
-
-for document in cursor:
-    print(document)
-
-so = stackexchange.Site(stackexchange.StackOverflow, sh.env('STACKAPP_KEY'))
-
-# Random user.
-print(so.user(4032278).display_name)
+print(str(len(trends)) + ' linhas incluídas!')
