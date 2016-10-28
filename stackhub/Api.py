f8664858aa1722119993dda70f67e167cf808bc8
Gustavo Marttos <marttosc@gmail.com>
2016-10-28 20:15:24 -0200

    Search repositories from API.

--- stackhub/Api.py
+++ stackhub/Api.py
@@ -4,6 +4,7 @@ import github
 
 import socket
 
+
 class Api(object):
     def __init__(self, id, key = None):
         self._id = id
@@ -23,13 +24,13 @@ class Github(Api):
     def __init__(self, token):
         Api.__init__(self, token)
 
-        Api.provider = github.Github(login_or_token=self.token[0])
+        Api.provider = github.Github(login_or_token=self.token[0], per_page=100)
 
-    def get_languages_via_repos(self):
+    def get_languages_via_repos(self, query, sort=github.GithubObject.NotSet, order=github.GithubObject.NotSet):
         languages = []
 
         try:
-            repositories = self.provider.get_repos()
+            repositories = self.provider.search_repositories(query, sort=sort, order=order)
 
             for repository in repositories:
                 _languages = repository.get_languages()
@@ -44,6 +45,9 @@ class Github(Api):
                         languages[index]['counter'] += 1
         except socket.timeout:
             print('Connection timeout. Try again later.')
+
+        except github.GithubException.UnknownObjectException as err:
+            print('An unknow error was throwed: {0}'.format(e))
 
         return languages
 
