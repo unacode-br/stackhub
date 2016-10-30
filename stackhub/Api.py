ce8a30ec5f41ddabb5efa499694d975b839a15c7
Gustavo Marttos <marttosc@gmail.com>
2016-10-29 23:27:58 -0200

    Implement the Stackoverflow class.

--- stackhub/Api.py
+++ stackhub/Api.py
@@ -1,6 +1,7 @@
 from stackhub import Helper
 
 import github
+import stackexchange
 
 import socket
 
@@ -26,7 +27,7 @@ class Github(Api):
 
         Api.provider = github.Github(login_or_token=self.token[0], per_page=100)
 
-    def get_languages_via_repos(self, query, sort=github.GithubObject.NotSet, order=github.GithubObject.NotSet):
+    def get_languages_from_repos(self, query, sort=github.GithubObject.NotSet, order=github.GithubObject.NotSet):
         languages = []
 
         try:
@@ -39,10 +40,16 @@ class Github(Api):
                     index = Helper.find_in_list(languages, 'language', language)
 
                     if index is None:
-                        languages.append({ 'language': language, 'counter': 1 })
+                        languages.append({
+                            'language': str(language).lower(),
+                            'language_original': language,
+                            'counter': 1,
+                            'repositories': [ repository.full_name ]
+                        })
 
                     if index is not None:
                         languages[index]['counter'] += 1
+                        languages[index]['repositories'].append(repository.full_name)
         except socket.timeout:
             print('Connection timeout. Try again later.')
 
@@ -54,4 +61,35 @@ class Github(Api):
 
 class Stackoverflow(Api):
     def __init__(self, token):
-        pass
+        Api.__init__(self, token)
+
+        Api.provider = stackexchange.Site(stackexchange.StackOverflow, self.token[0])
+
+    def get_tags_from_repos(self, from_date, to_date, order='desc', sort='votes'):
+        tags = []
+
+        from_date = Helper.date_to_unix(from_date)
+        to_date = Helper.date_to_unix(to_date)
+
+        questions = self.provider.search_advanced(fromdate=from_date, todate=to_date, pagesize=100, order=order, sort=sort)
+
+        for question in questions:
+            _tags = question.tags
+
+            for tag in _tags:
+                index = Helper.find_in_list(tags, 'tag', tag)
+
+                if index is None:
+                    tags.append({
+                        'tag': str(tag).lower(),
+                        'counter': 1,
+                        'score': question.score,
+                        'questions': [ question.id ]
+                    })
+
+                if index is not None:
+                    tags[index]['counter'] += 1
+                    tags[index]['score'] += question.score
+                    tags[index]['questions'].append(question.id)
+
+        return tags
--- stackhub/Helper.py
+++ stackhub/Helper.py
@@ -1,3 +1,6 @@
+import time
+from datetime import datetime
+
 def find_in_list(lst, key, value):
     """
     Find an index in a list.
@@ -6,3 +9,9 @@ def find_in_list(lst, key, value):
         return next(index for (index, x) in enumerate(lst) if x[key] == value)
     except:
         return None
+
+def date_to_unix(dt):
+    """
+    Convert a string date to an UNIX timestamp.
+    """
+    return time.mktime(datetime.strptime(dt, '%Y-%m-%d').timetuple())
