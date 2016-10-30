57cf6828d2f63cf2b47b473ef06ddb5efb7a3eaf
Gustavo Marttos <marttosc@gmail.com>
2016-10-29 23:58:59 -0200

    Add the learning curve method.

--- stackhub/Helper.py
+++ stackhub/Helper.py
@@ -1,3 +1,4 @@
+import math
 import time
 from datetime import datetime
 
@@ -15,3 +16,23 @@ def date_to_unix(dt):
     Convert a string date to an UNIX timestamp.
     """
     return time.mktime(datetime.strptime(dt, '%Y-%m-%d').timetuple())
+
+def learning_curve(z, n, curve, counter=5):
+    """
+    Calculate the learning curve five times.
+    """
+    _counter = 1
+
+    data = []
+
+    while _counter <= counter:
+        if _counter > 1:
+            curve = curve - (curve * 0.016)
+
+        lc = z * (math.pow(curve, math.log(n, 2)))
+
+        data.append(lc)
+
+        _counter += 1
+
+    return tuple(data)
