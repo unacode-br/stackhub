b0dd7e469093a681e32db5d23f1310a0b6fe79de
Gustavo Marttos <marttosc@gmail.com>
2016-10-30 01:03:08 -0200

    Update the example.

--- example.py
+++ example.py
@@ -1,7 +1,13 @@
 from stackhub.Stackhub import Stackhub
+from stackhub import Api
 
 sh = Stackhub()
 
-trends = sh.github_trends()
+github_api = Api.Github(sh.env('GITHUB_TOKEN'))
+stackoverflow_api = Api.Stackoverflow(sh.env('STACKAPP_KEY'))
 
-print(str(len(trends)) + ' linhas incluídas!')
+# Search from GitHub Developer Guide - https://developer.github.com/v3/search/
+github_data = github_api.get_languages_from_repos('your query here')
+
+# Advanced search from Stack Exchange API - https://api.stackexchange.com/docs/advanced-search
+stackoverflow_data = stackoverflow_api.get_tags_from_repos('2016-01-01', '2016-01-30')
