c211617737b519f5e3de1c8bac6fb5289088964a
Gustavo Marttos <marttosc@gmail.com>
2016-10-30 22:22:07 -0200

    Update the example.

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
 from stackhub.Stackhub import Stackhub
+from stackhub.Data import LearningCurve
 from stackhub import Api
 
 sh = Stackhub()
@@ -11,3 +12,37 @@ github_data = github_api.get_languages_from_repos('your query here')
 
 # Advanced search from Stack Exchange API - https://api.stackexchange.com/docs/advanced-search
 stackoverflow_data = stackoverflow_api.get_tags_from_repos('2016-01-01', '2016-01-30')
+
+# Add your github_data and stackoverflow_data to MongoDB here.
+
+learning_curve = LearningCurve(sh.db).process()
+
+result = learning_curve.get_result()
+
+"""
+`result` will return a list of dicts whith the following structure (e.g.):
+
+[
+    {
+        'language': {
+            'name': 'Python',
+            'slug': 'python',
+            'repositories': {
+                'total': 100
+            }
+        },
+        'tag': {
+            'tag': 'python',
+            'counter': 1024,
+            'score': 450
+        },
+        'points': [
+            { 'x': 1, 'y': 1, 'value': 450 },
+            { 'x': 2, 'y': 0.8, 'value': 143.27034502030915 },
+            ...
+        ]
+    },
+    ...
+]
+
+"""
